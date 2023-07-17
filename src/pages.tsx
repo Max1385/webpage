7c281f50721d4992ad675148848a3673acb0db65
Max <maxherrmann315@yahoo.com>
2023-07-17 16:03:35 +0200

    Added: - discord presence - adjustments - twitter, google drive link - typed.js added. - Profile pictures were exchanged - typed.js installed - Foundation stone for the website was laid.

--- src/pages.tsx
+++ src/pages.tsx
@@ -1,11 +1,16 @@
 import { css } from "@emotion/react";
 import { Title } from "react-head";
 import { Link } from "react-router-dom";
+import Typed from 'typed.js';
+import { useEffect, useRef } from "react";
 
 const BlobLumaSnow = new URL("./media/abloblumasnow.gif", import.meta.url);
 const GitHub = new URL("./media/github.svg", import.meta.url);
 const Mail = new URL("./media/mail.svg", import.meta.url);
-const YouTube = new URL("./media/youtube.svg", import.meta.url);
+// const YouTube = new URL("./media/youtube.svg", import.meta.url); / wird nicht mehr benötigt
+const Twitter = new URL("./media/twitter.svg", import.meta.url);
+const Drive = new URL("./media/drive.svg", import.meta.url);
+const Discord = new URL("./media/discord.svg", import.meta.url);
 
 const holder = css({
   padding: "2.5em",
@@ -14,33 +19,58 @@ const holder = css({
 const link = css({
   padding: "0.75em",
   "&:focus, &:hover": {
-    filter: "invert(0.5) sepia(1) hue-rotate(305deg) saturate(10)",
+    filter: "invert(0.5) sepia(1) hue-rotate(558deg) saturate(10)",
+    // Hier sind verschiedene Filterwerte angewendet, um den gewünschten dunkelblauen Farbton zu erzielen.
   },
   "@media (prefers-color-scheme: dark)": {
     filter: "invert(1)",
   },
-})
+});
+
+const autoType = css({
+  color: "#542CF2", // färben des textes
+});
 
 export function Home() {
+  useEffect(() => {
+    var typed = new Typed('#auto-type', {
+      strings: ["Server-Owner", "Bot-Developer", "Designer", "Moderator"],
+      typeSpeed: 80,
+      backSpeed: 80,
+      loop: true
+    });
+  }, []);
+
   return (
     <>
-      <Title>Home - LostLuma</Title>
+      <Title>Home - Max1385</Title>
       <img
         src={BlobLumaSnow.href}
         width="128"
         height="128"
-        alt="Luma lost in the snow!"
+        alt="Max1385 page"
       />
       <div css={holder}>
-        <a href="https://youtube.com/@LostLuma" title="YouTube" css={link}>
-          <img src={YouTube.href} alt="" />
-        </a>
-        <a href="https://github.com/LostLuma" title="GitHub" css={link}>
+        <a href="https://github.com/Max1385" title="GitHub" css={link}>
           <img src={GitHub.href} alt="" />
         </a>
-        <a href="mailto:lilly@lostluma.net" title="Email" css={link}>
+        <a href="https://twitter.com/realsucces1385" title="Twitter" css={link}>
+          <img src={Twitter.href} alt="" />
+        </a>
+        <a href="https://drive.google.com/drive/folders/1cqIJ08Fvtm4sjjIWxU6U0nz6ONPd9th_" title="My own Google Drive folder where I present a few of my self-made designs." css={link}>
+          <img src={Drive.href} alt="" />
+        </a>
+        <a href="mailto:max1385@outlook.de" title="Email" css={link}>
           <img src={Mail.href} alt="" />
         </a>
+        <div>
+          <a href='https://discord.com/users/770636457043034112' title="Discord">
+            <img src="https://discord.c99.nl/widget/theme-1/770636457043034112.png" style={{ padding: '5px' }} />
+          </a>
+        </div>
+      </div>
+      <div>
+        <h2>I'm a <span id="auto-type" css={autoType}></span></h2>
       </div>
     </>
   );
@@ -49,7 +79,7 @@ export function Home() {
 export function NotFound() {
   return (
     <>
-      <Title>Not Found - LostLuma</Title>
+      <Title>Not Found - Max1385</Title>
       <div>
         Looks like you took a wrong turn! <Link to="/">Go Home.</Link>
       </div>
